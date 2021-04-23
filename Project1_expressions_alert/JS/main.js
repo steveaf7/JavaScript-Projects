cc0013c3555616037aa14c15a45027196b0d1886
StevenFralix <steveaf7@gmail.com>
2021-04-23 09:16:06 -0700

    Basic JavaScript Projects Submission

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+window.alert("Welcome to my first JavaScript Page"); //creates a pop up window with the containing string.
+var firstSentence = "This green sentence should be " //defines the variable firstSentence and gives it a string value.
+var secondSentence = "connected to this red sentence" //defines the variable secondSentence and gives it a string value.
+var greenSentence = firstSentence.fontcolor("green"); //uses the fontcolor method on the variable firstSentence, defines variable greenSentence as the resulting value.
+var redSentence = secondSentence.fontcolor("red"); //uses the fontColor method on the variable secondSentence, defines new variable redSentence as the resulting value.
+document.write(greenSentence + redSentence); //concatenates greenSentence and redSentence and writes it to the screen.
+2 + 4 //This is an example of an expression.
