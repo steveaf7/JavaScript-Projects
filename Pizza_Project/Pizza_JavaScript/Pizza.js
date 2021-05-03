c71fae493b7b99a1faec10b2d68e137f89e43e5d
StevenFralix <steveaf7@gmail.com>
2021-05-03 09:57:02 -0700

    pizza menu push
    updated pizza menu to include veggie section, changed style of place order button, added comments to JavaScript

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -3,7 +3,7 @@ function getReceipt() {
     //line by line into a full receipt.
     var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0; //declares a variable to store the running total
-    var sizeTotal = 0;
+    var sizeTotal = 0; //declares a variable to store the price of the size of the pizza. 
     //creates a list of all the "size" elements
     var sizeArray = document.getElementsByClassName("size"); 
     //iterates through all "size" elements, if box is checked, add to new variable selectedSize
@@ -14,7 +14,7 @@ function getReceipt() {
             text1 = text1+selectedSize+"<br>"
         }
     }
-    //changes sizeTotal variable according to what was selected
+    //sets the price of the pizza, according to which pizza was selected.
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
@@ -26,15 +26,15 @@ function getReceipt() {
     } else if (selectedSize === "Extra Large Pizza") {
         sizeTotal = 16;
     }
-    runningTotal = sizeTotal; //assuming the size is the same as the price
-    console.log(selectedSize+" = $"+sizeTotal+".00");
-    console.log("size text1: "+text1);
+    runningTotal = sizeTotal; //sets the base price of the pizza as runningTotal.
+    console.log(selectedSize+" = $"+sizeTotal+".00"); //prints concatenated string to the console.
+    console.log("size text1: "+text1); //
     console.log("subtotal: $"+runningTotal+".00");
     getTopping(runningTotal,text1);
 };
 
 function getTopping(runningTotal,text1) {
-    var toppingTotal = 0; //declares a variable to store 
+    var toppingTotal = 0; //declares a variable to store total number of toppings for pricing.
     var selectedTopping = []; //creates an empty array to hold selected toppings.
     //gets all topping elements and adds to an array.
     var toppingArray = document.getElementsByClassName("toppings");
@@ -45,18 +45,18 @@ function getTopping(runningTotal,text1) {
             text1 = text1+toppingArray[j].value+"<br>"; //adds selected toppings to the receipt.
         }
     }
-    var toppingCount = selectedTopping.length; //
-    if (toppingCount > 1) {
-        toppingTotal = (toppingCount - 1);
-    } else {
+    var toppingCount = selectedTopping.length; //sets topping count equal to number of selected toppings
+    if (toppingCount > 1) { //as long as there are any toppings selected
+        toppingTotal = (toppingCount - 1); //change topping count to reflect discount.
+    } else { //if no toppings selected, no discount is applied.
         toppingTotal = 0;
     }
-    runningTotal = (runningTotal + toppingTotal);
-    console.log("total selected topping items: "+toppingCount);
+    runningTotal = (runningTotal + toppingTotal); //adds topping and pizza price together to get total price.
+    console.log("total selected topping items: "+toppingCount); //prints the topping count to the console.
     console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
     console.log("toppingtext1: "+text1);
     console.log("Purchase Total: "+"$"+runningTotal+".00");
-    document.getElementById("showText").innerHTML=text1;
+    document.getElementById("showText").innerHTML=text1; //gets element "showText" and changes it to display the value at text1
     document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+
-        runningTotal+".00"+"</strong></h3>";
+        runningTotal+".00"+"</strong></h3>"; //This show the total price next to the receipt, makes bold.
 };
